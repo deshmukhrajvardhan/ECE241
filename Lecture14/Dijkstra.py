65a6479ee789c85d86e111e11b3534647cb873f5
RAJVARDHAN SOMRAJ DESHMUKH <17696886+deshmukhrajvardhan@users.noreply.github.com>
2018-11-15 10:53:21 -0500

    Update Dijkstra.py

--- Lecture14/Dijkstra.py
+++ Lecture14/Dijkstra.py
@@ -1,4 +1,7 @@
-from Graph import Graph, Vertex, PriorityQueue
+#from Graph import Graph, Vertex
+#import queue as Q
+from pythonds.graphs import PriorityQueue, Graph, Vertex
+
 def dijkstra(aGraph,start):
     pq = PriorityQueue()
     start.setDistance(0)
@@ -12,3 +15,19 @@ def dijkstra(aGraph,start):
                 nextVert.setDistance( newDist )
                 nextVert.setPred(currentVert)
                 pq.decreaseKey(nextVert,newDist)
+        print(currentVert)
+    # for g in aGraph:
+    #     print(g)
+
+g = Graph()
+g.addEdge('V0','V1',5)
+g.addEdge('V0','V5',2)
+g.addEdge('V1','V2',4)
+g.addEdge('V2','V3',9)
+g.addEdge('V3','V4',7)
+g.addEdge('V3','V5',3)
+g.addEdge('V4','V0',1)
+g.addEdge('V5','V2',1)
+g.addEdge('V5','V4',8)
+
+dijkstra(g,g.getVertex('V0'))
